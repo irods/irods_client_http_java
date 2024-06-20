65567e7073beb7d462f63e9923238ff957dc4dd9
sam-i-am012 <samanthamau12@gmail.com>
2024-06-20 13:27:52 -0400

    No longer need to make seperate instances of IrodsClient for each user

---
+++ src/main/java/org/example/Example/Example.java
@@ -0,0 +1,37 @@
+package org.example.Example;
+
+import org.example.IrodsClient;
+import org.example.User;
+
+import java.io.IOException;
+
+public class Example {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        //http://52.91.145.195:8888/irods-http-api/0.3.0
+        String address = "52.91.145.195";
+        String port = "8888";
+        String version = "0.3.0";
+
+        // create client
+        IrodsClient client = new IrodsClient.Builder()
+                .address(address)
+                .port(port)
+                .version(version)
+                .build();
+
+        // Create users
+        User rods = new User("rods", "rods", client);
+        User alice = new User("alice", "alicepass", client);
+
+
+        client.collections().create(rods, "/tempZone/home/alice/test2", true);
+
+        client.collections().remove(rods, "/tempZone/home/alice/test2").noTrash().recurse().execute();
+        client.collections().remove(rods, "/tempZone/home/alice/test2").execute();
+
+
+        System.out.println("Token is: " + rods.getAuthToken());
+
+    }
+}
