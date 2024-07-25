eda3ed581eaafe32baeb0abf9b4d1db6db2197c3
sam-i-am012 <samanthamau12@gmail.com>
2024-07-25 14:50:50 -0400

    [#18] Removed try/catch block in modify_metadata for collections endpoint

--- src/main/java/org/example/Operations/CollectionOperations.java
+++ src/main/java/org/example/Operations/CollectionOperations.java
@@ -116,15 +116,10 @@ public class CollectionOperations {
     }
 
     public Response modify_permissions(String token, String lpath, List<ModifyPermissionsOperations> jsonParam,
-                                       OptionalInt admin) {
+                                       OptionalInt admin) throws JsonProcessingException {
         // Serialize the operations parameter to JSON
         ObjectMapper mapper = new ObjectMapper();
-        String operationsJson = null;
-        try {
-            operationsJson = mapper.writeValueAsString(jsonParam);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
+        String operationsJson = mapper.writeValueAsString(jsonParam);
 
         // contains parameters for the HTTP request
         Map<Object, Object> formData = new HashMap<>();
