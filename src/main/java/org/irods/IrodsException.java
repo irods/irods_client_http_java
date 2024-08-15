901cf6a85b31168d2001a19a754975aca90885b7
sam-i-am012 <samanthamau12@gmail.com>
2024-08-15 13:10:10 -0400

    [#91,#93] Throw JsonProcessingException in modifyPermissions and delete unused classes

--- src/main/java/org/irods/IrodsException.java
+++
@@ -1,7 +0,0 @@
-package org.irods;
-
-public class IrodsException extends Exception {
-    public IrodsException(String message) {
-        super(message);
-    }
-}
--- src/main/java/org/irods/util/IrodsException.java
+++
@@ -1,7 +0,0 @@
-package org.irods.util;
-
-public class IrodsException extends Exception {
-    public IrodsException(String message) {
-        super(message);
-    }
-}
